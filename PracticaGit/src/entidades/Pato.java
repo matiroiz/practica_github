d068afd983685579d9a25a810c35f4231413ed1d
Pablo Fernando Martinez <pablofmartinez@hotmail.com.ar>
2021-10-14 20:31:44 -0300

    Ingreso de Clases

--- PracticaGit/src/entidades/Pato.java
+++ PracticaGit/src/entidades/Pato.java
@@ -1,15 +1,53 @@
 package entidades;
+
 /**
  * Pablo Fernando Martinez
  */
 public class Pato {
 
     private String color;
-    private String grande;
+    private String tamaño;
+    private String especie;
+    private boolean volador;
+    private String nombre;
+    private Integer edad;
 
-   public Pato(){
-    
-} 
+    public Pato() {
+
+    }
+
+    public Pato(String color, String tamaño, String especie, boolean volador, String nombre, Integer edad){
+        this.color=color;
+        this.tamaño= tamaño;
+        this.especie = especie;
+        this.volador = volador;
+        this.nombre = nombre;
+        this.edad = edad;
+        
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public boolean getVolador() {
+        return volador;
+    }
+
+    public void setVolador(boolean volador) {
+        this.volador = volador;
+    }
 
     public String getColor() {
         return color;
@@ -19,12 +57,20 @@ public class Pato {
         this.color = color;
     }
 
-    public String getGrande() {
-        return grande;
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setGrande(String grande) {
-        this.grande = grande;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
     }
 
 }
